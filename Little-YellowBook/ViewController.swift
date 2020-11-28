cd827cda970f48d10a7d7138099b834cf11d8196
kidd <1366047541@qq.com>
2020-11-28 14:25:13 +0800

    做了AppIcon的图标和TabBarController

--- Little-YellowBook/ViewController.swift
+++ Little-YellowBook/ViewController.swift
@@ -11,6 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        tabBarController?.tabBar.barTintColor = .black
         // Do any additional setup after loading the view.
     }
 
